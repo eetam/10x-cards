c6051cf9ff46b612e578f1b3590e8679707fc64b
Mateusz Calik <mateuszcalik89@gmail.com>
2025-12-11 00:54:15 +0100

    fix: remove .wrangler from repo and add to gitignore

--- .wrangler/tmp/dev-LSJweH/pages/_image.astro.mjs
+++
@@ -1,3 +0,0 @@
-globalThis.process ??= {}; globalThis.process.env ??= {};
-export { a as page } from '../chunks/image-endpoint__jgpzmWV.mjs';
-export { r as renderers } from '../chunks/_@astro-renderers_D9WLdofW.mjs';
